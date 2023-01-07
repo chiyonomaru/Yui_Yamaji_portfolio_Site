5c9dc6891c530b9630c736c5933b9bfa8f6a2d2f
ゆい <82437352+chiyonomaru@users.noreply.github.com>
2023-01-07 16:57:07 +0900

    Update script.js
    Signed-off-by: ゆい <82437352+chiyonomaru@users.noreply.github.com>

--- script.js
+++ script.js
@@ -60,3 +60,83 @@ jQuery(function() {
     return false;
   });
 });
+
+
+// Window幅に応じたメニュー表示の変更
+function ShowAndHide(win){
+  if(win < 769){
+    $("#button").show();
+    $("#lists").hide();
+  }else{
+    $("#button").hide();
+    $("#lists").show();
+  }
+}
+
+$( function(){
+
+  // 初めてページを開いた時の状態チェック
+  ShowAndHide($(window).width());
+
+  // Windowサイズが変更された時の状態チェック
+  $(window).resize(function(){
+    ShowAndHide($(window).width());
+  });
+
+  // メニューボタンクリック時のトグル動作
+  $("#button").click( function () {
+    $("#lists").slideToggle();
+  });
+
+});
+
+
+//loading
+$(window).on('load',function(){
+  $("#loading").delay(2000).fadeOut('slow');
+  $("#loading_inner").delay(1600).fadeOut('slow');
+});
+
+////////////////////////////////////////////////
+
+jQuery(function() {
+  var appear = false;
+  var pagetop = $('#page_top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'bottom': '50px' //下から50pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'bottom': '-50px' //下から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+});
+
+
+//scroll_effect
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 150;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
