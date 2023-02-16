165925769cce2394a8111b366838eee394ae2d25
ゆい <82437352+chiyonomaru@users.noreply.github.com>
2023-02-17 01:35:24 +0900

    Update 5-17.js
    Signed-off-by: ゆい <82437352+chiyonomaru@users.noreply.github.com>

--- js/5-17.js
+++ js/5-17.js
@@ -1,6 +1,6 @@
 
 $("#particle").particleText({
 text: "POSTER"// 表示させたいテキスト。改行の場合は<br>追加
-colors:colors:["#000"],// パーティクルの色を複数指定可能
+colors:["#000"],// パーティクルの色を複数指定可能
 speed: "high", // の3つから粒子が集まる速さを選択
 });
