eae22cbf804e4e9b48e4c380ed7b283ff12195a4
ゆい <82437352+chiyonomaru@users.noreply.github.com>
2023-02-17 15:29:21 +0900

    Update 5-17.js
    Signed-off-by: ゆい <82437352+chiyonomaru@users.noreply.github.com>

--- js/5-17.js
+++ js/5-17.js
@@ -3,7 +3,7 @@ function particleTextAnime() {
 
 $("#particle").particleText({
 		text: "poster", // 表示させたいテキスト。改行の場合は<br>追加
-		colors:["#000"], // パーティクルの色を複数指定可能
+		colors:["#444"], // パーティクルの色を複数指定可能
 		speed: "high",  // slow, middle, high の3つから粒子が集まる速さを選択
 	});
 }
